ca62cc8c1b88e470eac3a69f3214049ddd72017c
MikouRay <mikouray@gmail.com>
2023-03-27 12:12:18 +0800

    Meet quiz 3 requirments

---
+++ SideScrollingGame/Assets/Scripts/Base/AreaTrigger2D.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AreaTrigger2D : MonoBehaviour
+{
+    [Header ("Settings")]
+    [SerializeField] public LayerMask layerMask;
+    
+    [Header ("No arguments")]
+    [SerializeField] public UnityEvent onEnter;
+    [SerializeField] public UnityEvent onStay;
+    [SerializeField] public UnityEvent onExit;
+
+    [Header ("With Collider")]
+    [SerializeField] public UnityEvent<Collider2D> onEnter_collider;
+    [SerializeField] public UnityEvent<Collider2D> onStay_collider;
+    [SerializeField] public UnityEvent<Collider2D> onExit_collider;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (GameManager.InLayerMask(other.gameObject, layerMask)) {
+            onEnter.Invoke();
+            onEnter_collider.Invoke(other);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (GameManager.InLayerMask(other.gameObject, layerMask)) {
+            onStay.Invoke();
+            onStay_collider.Invoke(other);
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other) {
+        if (GameManager.InLayerMask(other.gameObject, layerMask)) {
+            onExit.Invoke();
+            onExit_collider.Invoke(other);
+        }
+    }
+}
